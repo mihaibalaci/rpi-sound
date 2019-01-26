2a3fd2fa15d36fbf0cf1a9cc439f7d0a17b05afb
mihaibalaci <mihai.balaci@gmail.com>
2019-01-26 21:56:32 +0200

    modify sound.sh and install

--- basic-install.sh
+++ basic-install.sh
@@ -10,7 +10,7 @@ border()
 
 border 'Downloading Sound File'
 
-wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/Sound.sh -O /usr/bin/Sound.sh
+wget https://github.com/mihaibalaci/rpi-sound/blob/master/Sound.sh -O /usr/bin/Sound.sh
 chmod 755 /usr/bin/Sound.sh
 
 border 'Increasing Sound Group Priority'
